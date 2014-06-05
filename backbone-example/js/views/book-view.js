07b3e58a6d71cfb09b2cf66a3daae77ffb7105a6
Gloria Palma <ing.gloriapalmagonzalez@gmail.com>
2014-06-05 14:24:43 -0500

    Method save

--- backbone-example/js/views/book-view.js
+++ backbone-example/js/views/book-view.js
@@ -7,12 +7,16 @@ var app = app || {};
 		tagName:  'li',
 
 		events: {
-			'click .edit': 'edit'
+			'click .edit': 'edit',
+			'click .save': 'save'
 		},
 
 		template: _.template($('#item').html()),
 
 		initialize: function () { 
+			this.$title = this.$('#edit-title');
+			this.$pages = this.$('#edit-pages');
+			this.$read = $('#edit-read');
 		},
 
 		render: function () {
@@ -27,6 +31,17 @@ var app = app || {};
 			this.$el.find('.book-labels').toggle('.hide');
 			this.$el.find('.book-form').removeClass('hide');
 			this.$el.find('.title').first().focus();
+		}
+		save: function(){
+			//Define model 
+			var editBook=this.model;
+			//Change value
+			editBook.set('title',this.$title.val().trim());
+			editBook.set('pages',this.$pages.val().trim());
+			editBook.set('read',this.$read.is(':checked'));
+			//Change View
+			this.$el.find('.book-labels').removeClass('.hide');
+			this.$el.find('.book-form').toggle('hide');
 		}
 	});
 })(jQuery);
